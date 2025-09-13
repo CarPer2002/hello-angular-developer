29d94d7abbabc1ca6114ee1e423bb08e170922d0
andre <andrepv2003@gmai.com>
2025-09-13 17:05:50 -0500

    feat(greetings):  add main behavior.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DeveloperGreeting} from './greetings/presentation/components/developer-greeting/developer-greeting.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    DeveloperGreeting
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('hello-angular-developer');
+  public firstName: string = '';
+  public lastName: string = '';
+
+  public updateRegisteredDeveloperInfo(developer: {firstName: string, lastName: string}){
+    this.firstName = developer.firstName;
+    this.lastName = developer.lastName;
+  }
+
+  public resetRegisteredDeveloperInfo(){
+    this.firstName = '';
+    this.lastName = '';
+  }
 }
